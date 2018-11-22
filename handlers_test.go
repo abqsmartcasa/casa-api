aa4560eb2ac89a7d0275e1fb8d69c40c0daa9c43
Russ Biggs <russbiggs@gmail.com>
2018-11-22 10:06:31 -0700

    refactored handlers test to table-driven

--- handlers_test.go
+++ handlers_test.go
@@ -82,98 +82,37 @@ func (mdb *mockDB) GetReport(lang interface{}, report models.Report) (*models.Re
 	return c, nil
 }
 
-func TestParagraphs(t *testing.T) {
-	rr := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/paragraphs", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestHandlers(t *testing.T) {
 	router := mux.NewRouter()
 	env := Env{db: &mockDB{}}
 	router.HandleFunc("/paragraphs", env.paragraphs)
-	router.ServeHTTP(rr, req)
-	expected := "{\"data\":[{\"id\":42,\"paragraph_number\":42,\"paragraph_title\":\"test\",\"paragraph_text\":\"test\"}]}"
-	if expected != rr.Body.String() {
-		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rr.Body.String())
-	}
-}
-
-func TestParagraph(t *testing.T) {
-	rr := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/paragraphs/13", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	router := mux.NewRouter()
-	env := Env{db: &mockDB{}}
 	router.HandleFunc("/paragraphs/{key}", env.paragraph)
-	router.ServeHTTP(rr, req)
-	expected := "{\"data\":{\"id\":13,\"paragraph_number\":13,\"paragraph_title\":\"test\",\"paragraph_text\":\"test\"}}"
-	if expected != rr.Body.String() {
-		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rr.Body.String())
-	}
-}
-
-func TestCompliances(t *testing.T) {
-	rr := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/compliances", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	router := mux.NewRouter()
-	env := Env{db: &mockDB{}}
 	router.HandleFunc("/compliances", env.compliances)
-	router.ServeHTTP(rr, req)
-	expected := "{\"data\":[{\"id\":1,\"report_id\":2,\"paragraph_id\":3,\"primary_compliance\":\"In Compliance\",\"operational_compliance\":\"Not In Compliance\",\"secondary_compliance\":\"Not In Compliance\"}]}"
-	if expected != rr.Body.String() {
-		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rr.Body.String())
-	}
-}
-
-func TestCompliance(t *testing.T) {
-	rr := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/compliances/13", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	router := mux.NewRouter()
-	env := Env{db: &mockDB{}}
 	router.HandleFunc("/compliances/{key}", env.compliance)
-	router.ServeHTTP(rr, req)
-	expected := "{\"data\":{\"id\":13,\"report_id\":2,\"paragraph_id\":3,\"primary_compliance\":\"In Compliance\",\"operational_compliance\":\"Not In Compliance\",\"secondary_compliance\":\"Not In Compliance\"}}"
-	if expected != rr.Body.String() {
-		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rr.Body.String())
-	}
-}
-
-func TestReports(t *testing.T) {
-	rr := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/reports", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	router := mux.NewRouter()
-	env := Env{db: &mockDB{}}
 	router.HandleFunc("/reports", env.reports)
-	router.ServeHTTP(rr, req)
-	expected := "{\"data\":[{\"id\":1,\"report_name\":\"IMR-1\",\"report_title\":\"Monitor's First Report\",\"publish_date\":\"2015-11-23\",\"period_begin\":\"2015-02-01\",\"period_end\":\"2015-05-31\"}]}"
-	if expected != rr.Body.String() {
-		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rr.Body.String())
-	}
-}
-
-func TestReport(t *testing.T) {
-	rr := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/reports/1", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	router := mux.NewRouter()
-	env := Env{db: &mockDB{}}
 	router.HandleFunc("/reports/{key}", env.report)
-	router.ServeHTTP(rr, req)
-	expected := "{\"data\":{\"id\":1,\"report_name\":\"IMR-1\",\"report_title\":\"Monitor's First Report\",\"publish_date\":\"2015-11-23\",\"period_begin\":\"2015-02-01\",\"period_end\":\"2015-05-31\"}}"
-	if expected != rr.Body.String() {
-		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rr.Body.String())
+
+	tests := []struct {
+		description string
+		URL         string
+		expected    string
+	}{
+		{"all paragraphs", "/paragraphs", "{\"data\":[{\"id\":42,\"paragraph_number\":42,\"paragraph_title\":\"test\",\"paragraph_text\":\"test\"}]}"},
+		{"paragraph by key", "/paragraphs/13", "{\"data\":{\"id\":13,\"paragraph_number\":13,\"paragraph_title\":\"test\",\"paragraph_text\":\"test\"}}"},
+		{"all compliances", "/compliances", "{\"data\":[{\"id\":1,\"report_id\":2,\"paragraph_id\":3,\"primary_compliance\":\"In Compliance\",\"operational_compliance\":\"Not In Compliance\",\"secondary_compliance\":\"Not In Compliance\"}]}"},
+		{"compliance by key", "/compliances/13", "{\"data\":{\"id\":13,\"report_id\":2,\"paragraph_id\":3,\"primary_compliance\":\"In Compliance\",\"operational_compliance\":\"Not In Compliance\",\"secondary_compliance\":\"Not In Compliance\"}}"},
+		{"all reports", "/reports", "{\"data\":[{\"id\":1,\"report_name\":\"IMR-1\",\"report_title\":\"Monitor's First Report\",\"publish_date\":\"2015-11-23\",\"period_begin\":\"2015-02-01\",\"period_end\":\"2015-05-31\"}]}"},
+		{"report by key", "/reports/1", "{\"data\":{\"id\":1,\"report_name\":\"IMR-1\",\"report_title\":\"Monitor's First Report\",\"publish_date\":\"2015-11-23\",\"period_begin\":\"2015-02-01\",\"period_end\":\"2015-05-31\"}}"},
+	}
+	for _, test := range tests {
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", test.URL, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(rr, req)
+		if test.expected != rr.Body.String() {
+			t.Errorf("\n%v\n...expected = %v\n...obtained = %v", test.description, test.expected, rr.Body.String())
+		}
 	}
 }
